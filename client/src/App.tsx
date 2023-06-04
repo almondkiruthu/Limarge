45dc18f7fd39743415a0719c4d21b130b22435c8
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-06-05 00:16:44 +0300

    Comfigured prettier

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,9 @@
-
-
 const App = () => {
   return (
     <div>
-      <h1 className="bg-yellow-500">
-        Hello World!!
-      </h1>
+      <h1 className="bg-yellow-500">Hello World!!</h1>
     </div>
-  )
-}
+  );
+};
 
 export default App;
