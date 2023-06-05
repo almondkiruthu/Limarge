6e755e34ef9f10c46498362fd0e6b4daf7d21815
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-06-05 21:43:44 +0300

    Created the navbar component and added the logo

---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,33 @@
+const Navbar = () => {
+  return (
+    <>
+      <div>
+        <header className="bg-customBackground">
+          <nav>
+            <div className="flex">
+              <div className="mr-2">
+                <img src="./Navbar/Logo.svg" alt="logo" />
+              </div>
+
+              <p className="text-xl font-semibold font-inter">LIMARGE</p>
+            </div>
+            <div>
+              <ul className="font-inter text-customBlue flex items-center gap-[80px]">
+                <li>Home</li>
+                <li>Services</li>
+                <li>Stories</li>
+                <li>About Us</li>
+                <li>Company</li>
+              </ul>
+            </div>
+            <div className="font-inter text-customBlue">
+              <button className="p-2 border-customOrange border-2 rounded-lg">Contact us</button>
+            </div>
+          </nav>
+        </header>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
