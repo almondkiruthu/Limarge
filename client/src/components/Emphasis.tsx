a1dd5093fe78879e6916f9e31f1314b9cd5fb111
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-06-06 17:03:27 +0300

    Added the hero emphasis component image

--- client/src/components/Emphasis.tsx
+++ client/src/components/Emphasis.tsx
@@ -1,8 +1,26 @@
 const Emphasis = () => {
   return (
     <>
-      <div>
-        
+      <div className="bg-customBlue pl-56 text-white">
+        <div>
+          <h2 className="font-archivo font-semibold text-[58px] ">
+            Unlocking <br /> Potential. Driving <br /> Success.
+          </h2>
+        </div>
+        <div className="pt-4">
+          <p className="font-inter text-base">
+            Experience transformative consultancy solutions <br /> tailored to
+            your unique industry needs.
+          </p>
+        </div>
+        <div className="font-inter pt-8">
+          <button className="contact-us-btn px-8 py-3 rounded-[12px] font-medium text-white text-center">
+            Contact us
+          </button>
+        </div>
+        <div>
+            
+        </div>
       </div>
     </>
   );
