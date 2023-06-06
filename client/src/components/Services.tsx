08383acdbc90b92da742d58f2c0256fb618be30a
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-06-06 19:48:33 +0300

    Created the services component

--- client/src/components/Services.tsx
+++ client/src/components/Services.tsx
@@ -1,63 +1,22 @@
+import Servicecards from "./Servicecards";
+
 const Services = () => {
   return (
     <>
-      <div className="bg-customBlue text-white pt-20">
+      <div className="bg-customBlue text-white pt-20 relative">
         <div>
-          <h2 className="font-archivo text-[58px] text-center font-semibold">
+          <h2 className="font-archivo text-[58px] text-center font-semibold pb-14">
             Our Main <br /> Services
           </h2>
         </div>
-        <div>
-          <div className="education-card">
-            <p className="font-archivo text-xl">Education</p>
-            <div>
-              <p className="font-inter text-base">We specialize in.</p>
-            </div>
-            <div className="flex">
-              {" "}
-              <div>
-                <p className="font-inter text-base">01</p>
-              </div>
-              <div>
-                <p className="font-inter font-medium text-base">
-                  Masters and PhD proposal and <br /> thesis development.
-                </p>
-              </div>
-              <div>
-                <hr className="bg-white opacity-10" />
-              </div>
-            </div>
-            <div className="flex">
-              {" "}
-              <div>
-                <p className="font-inter text-base">02</p>
-              </div>
-              <div>
-                <p className="font-inter font-medium text-base">
-                  Research methodology and <br />
-                  data analysis guidance
-                </p>
-              </div>
-              <div>
-                <hr className="bg-white opacity-10" />
-              </div>
-            </div>
-            <div className="flex">
-              {" "}
-              <div>
-                <p className="font-inter text-base">03</p>
-              </div>
-              <div>
-                <p className="font-inter font-medium text-base">
-                Customized mentoring and <br />
-                coaching
-                </p>
-              </div>
-              <div>
-                <hr className="bg-white opacity-10" />
-              </div>
-            </div>
-          </div>
+        <Servicecards />
+        <div className="absolute right-[300px] top-4">
+          <img src="/Servicesassets/Star.svg" alt="star" />
+        </div>
+        <div className="font-inter pt-20 text-center">
+          <button className="contact-us-btn px-8 py-3 rounded-[12px] font-medium text-white text-center">
+            Contact us
+          </button>
         </div>
       </div>
     </>
