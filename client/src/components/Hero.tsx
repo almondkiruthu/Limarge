4be22c4c7e4e8d4f0c4f3bebe492cb1fe0d49e1e
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-06-06 15:15:50 +0300

    Edited the hero assets

--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -2,13 +2,17 @@ const Hero = () => {
   return (
     <>
       <div className="ml-56 mt-6">
-        <div className="flex items-baseline font-extrabold text-customBlue gap-[10px]">
+        <div className="flex items-baseline font-extrabold text-customBlue">
           <h1 className="font-archivo text-[98px]">We elevate and</h1>
-          <img src="./Hero/HeroTextImage1.svg" />
+          <div className="px-3">
+            <img src="./Hero/HeroTextImage1.svg" />
+          </div>
           <h1 className="font-archivo text-[98px]">enrich</h1>
         </div>
         <div className="flex items-baseline font-extrabold text-customBlue">
-          <img src="./Hero/HeroTextImage2.svg" />
+          <div className="pr-7">
+            <img src="./Hero/HeroTextImage2.svg" />
+          </div>
           <h1 className="font-archivo text-[98px]">change across industries</h1>
         </div>
         <div className="pt-5">
@@ -17,8 +21,10 @@ const Hero = () => {
             consultancy at your <br /> service.
           </p>
         </div>
-        <div className="font-inter pt-9" >
-          <button className="contact-us-btn px-8 py-3 rounded-[12px] font-medium text-white text-center">Contact us</button>
+        <div className="font-inter pt-9">
+          <button className="contact-us-btn px-8 py-3 rounded-[12px] font-medium text-white text-center">
+            Contact us
+          </button>
         </div>
       </div>
     </>
