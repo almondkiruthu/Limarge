ad5be6b3f32397db3d6203586d84bf4a41f70c3d
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-06-06 14:06:44 +0300

    Created the hero component

---
+++ client/src/components/Hero.tsx
@@ -0,0 +1,22 @@
+const Hero = () => {
+  return (
+    <>
+      <div>
+        <div>
+          <h1>We elevate and enrich change across industries</h1>
+        </div>
+        <div>
+          <p>
+            Unleash growth potential. Empower <br/> franchisees. Expert consultancy at
+            your <br /> service.
+          </p>
+        </div>
+        <div>
+            <button>Contact us</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Hero;
