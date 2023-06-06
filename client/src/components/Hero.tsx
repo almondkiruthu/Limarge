6936f1bc5536573c3eb256acb2023322b8f5295e
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-06-06 15:55:58 +0300

    Created the HeroExtension Component and addded all it's assets

--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -1,17 +1,20 @@
+import Heroextension from "./HeroExtension";
+
+
 const Hero = () => {
   return (
     <>
-      <div className="ml-56 mt-6">
+      <div className="ml-56 mt-6 relative">
         <div className="flex items-baseline font-extrabold text-customBlue">
           <h1 className="font-archivo text-[98px]">We elevate and</h1>
           <div className="px-3">
-            <img src="./Hero/HeroTextImage1.svg" />
+            <img src="./Hero/HeroTextImage1.svg" alt="HeroTextImage1" />
           </div>
           <h1 className="font-archivo text-[98px]">enrich</h1>
         </div>
         <div className="flex items-baseline font-extrabold text-customBlue">
           <div className="pr-7">
-            <img src="./Hero/HeroTextImage2.svg" />
+            <img src="./Hero/HeroTextImage2.svg" alt="HeroTextImage2"/>
           </div>
           <h1 className="font-archivo text-[98px]">change across industries</h1>
         </div>
@@ -26,7 +29,11 @@ const Hero = () => {
             Contact us
           </button>
         </div>
+        <div className="absolute right-0 top-[150px]">
+            <img src="./Hero/VibrantHeroLine.svg" alt="line" />
+        </div>
       </div>
+      <Heroextension />
     </>
   );
 };
---
+++ client/src/components/Heroextension.tsx
@@ -0,0 +1,14 @@
+
+const Heroextension = () => {
+  return (
+    <>
+      <div className="bg-customBlue">
+        <div className="flex">
+          
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Heroextension;
