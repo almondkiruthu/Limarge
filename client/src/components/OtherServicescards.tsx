3e77ac3caf4c9164ca3c7cc241b521909acaea7d
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-06-07 16:55:07 +0300

    Finished up theother services section with a new component to hold it's cards

---
+++ client/src/components/OtherServicescards.tsx
@@ -0,0 +1,225 @@
+const OtherServicescards = () => {
+  return (
+    <>
+      <div className="flex gap-14 ml-[256px] text-white">
+        {/* Education card */}
+        <div className="education-card pt-10 pr-10 pl-10 pb-0 rounded-[20px] mt-20">
+          <p className="font-archivo text-xl font-semibold pb-3">Education</p>
+          <div>
+            <p className="font-inter text-base pb-3">We specialize in.</p>
+          </div>
+          <div className="flex pl-2">
+            {" "}
+            <div>
+              <p className="font-inter text-base opacity-60">01</p>
+            </div>
+            <div className="pl-3">
+              <p className="font-inter font-medium text-base">
+                Masters and PhD proposal and <br /> thesis development.
+              </p>
+              <div className="pt-2">
+                <img
+                  src="/Servicesassets/eduCardunderline.svg"
+                  alt="underline"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="flex pl-2 pt-2">
+            {" "}
+            <div>
+              <p className="font-inter text-base opacity-60">02</p>
+            </div>
+            <div className="pl-3">
+              <p className="font-inter font-medium text-base">
+                Research methodology and <br />
+                data analysis guidance
+              </p>
+              <div className="pt-2">
+                <img
+                  src="/Servicesassets/eduCardunderline.svg"
+                  alt="underline"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="flex pl-2 pt-2">
+            {" "}
+            <div>
+              <p className="font-inter text-base opacity-60">03</p>
+            </div>
+            <div className="pl-3">
+              <p className="font-inter font-medium text-base">
+                Customized mentoring and <br />
+                coaching
+              </p>
+              <div className="pt-2">
+                <img
+                  src="/Servicesassets/eduCardunderline.svg"
+                  alt="underline"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* Business card */}
+
+        <div className="business-card pt-5 pr-10 pl-10 pb-10 rounded-[20px] ml-32">
+          <p className="font-archivo text-[28px] font-semibold pb-3">
+            Business and <br /> Market Research
+          </p>
+          <div>
+            <p className="font-inter text-base pb-5">
+              We specialize in developing.
+            </p>
+          </div>
+          <div className="flex pl-2">
+            {" "}
+            <div>
+              <p className="font-inter text-base">01</p>
+            </div>
+            <div className="pl-3">
+              <p className="font-inter font-medium text-base">Market surveys</p>
+              <div className="pt-2">
+                <img
+                  src="/Servicesassets/eduCardunderline.svg"
+                  alt="underline"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="flex pl-2 pt-2">
+            {" "}
+            <div>
+              <p className="font-inter text-base">02</p>
+            </div>
+            <div className="pl-3">
+              <p className="font-inter font-medium text-base">
+                Data collection and analysis
+              </p>
+              <div className="pt-2">
+                <img
+                  src="/Servicesassets/eduCardunderline.svg"
+                  alt="underline"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="flex pl-2 pt-2">
+            {" "}
+            <div>
+              <p className="font-inter text-base">03</p>
+            </div>
+            <div className="pl-3">
+              <p className="font-inter font-medium text-base">
+                Identifying market trends
+              </p>
+              <div className="pt-2">
+                <img
+                  src="/Servicesassets/eduCardunderline.svg"
+                  alt="underline"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="flex pl-2 pt-2">
+            {" "}
+            <div>
+              <p className="font-inter text-base">04</p>
+            </div>
+            <div className="pl-3">
+              <p className="font-inter font-medium text-base">
+                Competitive analysis
+              </p>
+              <div className="pt-2">
+                <img
+                  src="/Servicesassets/eduCardunderline.svg"
+                  alt="underline"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="flex pl-2 pt-2">
+            {" "}
+            <div>
+              <p className="font-inter text-base">05</p>
+            </div>
+            <div className="pl-3">
+              <p className="font-inter font-medium text-base">
+                Customized research solutions <br />
+                tailored to client needs
+              </p>
+              <div className="pt-2">
+                <img
+                  src="/Servicesassets/eduCardunderline.svg"
+                  alt="underline"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* Finance and Tax mangaement card */}
+        <div className="education-card p-10 rounded-[20px] mt-20 ml-24">
+          <p className="font-archivo text-xl font-semibold pb-3">
+            Financial and Tax <br /> Management
+          </p>
+          <div>
+            <p className="font-inter text-base pb-3">We specialize in.</p>
+          </div>
+          <div className="flex pl-2">
+            {" "}
+            <div>
+              <p className="font-inter text-base opacity-60">01</p>
+            </div>
+            <div className="pl-3">
+              <p className="font-inter font-medium text-base">
+                Financial management practices <br /> and strategies
+              </p>
+              <div className="pt-2">
+                <img
+                  src="/Servicesassets/eduCardunderline.svg"
+                  alt="underline"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="flex pl-2 pt-2">
+            {" "}
+            <div>
+              <p className="font-inter text-base opacity-60">02</p>
+            </div>
+            <div className="pl-3">
+              <p className="font-inter font-medium text-base">
+                Working capital management
+              </p>
+              <div className="pt-2">
+                <img
+                  src="/Servicesassets/eduCardunderline.svg"
+                  alt="underline"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="flex pl-2 pt-2">
+            {" "}
+            <div>
+              <p className="font-inter text-base opacity-60">03</p>
+            </div>
+            <div className="pl-3">
+              <p className="font-inter font-medium text-base">Credit control</p>
+              <div className="pt-2">
+                <img
+                  src="/Servicesassets/eduCardunderline.svg"
+                  alt="underline"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default OtherServicescards;
