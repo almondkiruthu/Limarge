f6c99bd84dfed5d6e110b7f857682d6f44e7d102
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-06-07 15:27:18 +0300

    Added the Teams component

---
+++ client/src/components/Team.tsx
@@ -0,0 +1,20 @@
+const Team = () => {
+  return (
+    <>
+      <div className="pt-[480px]">
+        <div>
+          <p className="font-archivo text-[58px] text-customBlue text-center font-semibold ">
+            Comprised of diverse <br /> minds
+          </p>
+        </div>
+        <div className="flex">
+          <div className="bg-cutomBlue rounded-[12px]">
+
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Team;
