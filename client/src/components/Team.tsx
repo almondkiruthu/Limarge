aa9ca423667496a9d05b7fc83fce6f8b2fc0ed05
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-06-07 16:13:58 +0300

    Created the teams cards

--- client/src/components/Team.tsx
+++ client/src/components/Team.tsx
@@ -7,9 +7,45 @@ const Team = () => {
             Comprised of diverse <br /> minds
           </p>
         </div>
-        <div className="flex">
-          <div className="bg-cutomBlue rounded-[12px]">
+        <div className="flex gap-28 ml-[480px] pt-12">
+          <div className="bg-customBlue rounded-[12px] pt-5 pr-5 pl-5 pb-4">
+            <div className="bg-[#FE8650] rounded-[12px]">
+              <div className="ml-[75px]">
+                <img src="/Teamassets/Consultant.svg" alt="consultant" />
+              </div>
+            </div>
+            <div className="text-white pt-2">
+              <p className="font-inter text-[26px] pb-12">
+                Dr. Jane Efery Wanyoike <br /> PhD, CPA (K)
+              </p>
+              <p className="font-inter text-[32px]">LEAD CONSULTANT</p>
+            </div>
+            <div>
+              <button>
+                <img src="/Teamassets/linkedinbtn.svg" alt="linkedin" />
+              </button>
+            </div>
+          </div>
 
+          <div className="bg-customBlue rounded-[12px] pt-5 pr-5 pl-5 pb-2">
+            <div className="bg-[#FE8650] rounded-[12px]">
+              <div className="ml-[75px]">
+                <img src="/Teamassets/Consultant.svg" alt="consultant" />
+              </div>
+            </div>
+            <div className="text-white pt-2">
+              <p className="font-inter text-[26px] pb-2">
+                Dr. Margaret.G PhD HRM, <br /> MA Counseling psychology,
+                <br />
+                B/ed(special education)
+              </p>
+              <p className="font-inter text-[32px]">LEAD CONSULTANT</p>
+            </div>
+            <div>
+              <button>
+                <img src="/Teamassets/linkedinbtn.svg" alt="linkedin" />
+              </button>
+            </div>
           </div>
         </div>
       </div>
