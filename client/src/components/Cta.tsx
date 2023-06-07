1a52b80289dfbb5abd150cb92e1bccb68d729815
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-06-07 18:03:42 +0300

    Created the CTA section

--- client/src/components/Cta.tsx
+++ client/src/components/Cta.tsx
@@ -3,8 +3,18 @@ import Testimonials from "./Testimonials";
 const Cta = () => {
   return (
     <>
-      <div className="bg-customBlue rounded-t-[100px] relative">
+      <div className="bg-customBlue rounded-t-[100px] relative pb-[500px] mt-60">
         <Testimonials />
+      </div>
+      <div className="text-white bg-customBlue text-center pt-20">
+        <h2 className="font-archivo text-[58px] font-semibold">
+          Have an Idea? Let's <br /> consult and scale it <br /> together
+        </h2>
+        <div className="font-inter pt-24 text-center">
+          <button className="contact-us-btn px-8 py-3 rounded-[12px] font-medium text-white text-center">
+            Book a demo
+          </button>
+        </div>
       </div>
     </>
   );
--- client/src/components/Testimonials.tsx
+++ client/src/components/Testimonials.tsx
@@ -1,15 +1,15 @@
 const Testimonials = () => {
   return (
     <>
-      <div className="testimonial-card text-white">
+      <div className="testimonial-card text-white absolute rounded-[40px] z-10 p-20 left-[274px] bottom-[0]">
         <div>
           <h3 className="font-archivo font-semibold text-[58px]">
             Igniting success through <br /> consultancy expertise
           </h3>
         </div>
-        <div className="flex">
+        <div className="flex gap-72">
           <div className="font-archivo text-[28px]">
-            <p>
+            <p className="pb-8 pt-5">
               Limerage Consultancy has been an invaluable partner in our
               <br /> journey towards success. Their expert advice and strategic{" "}
               <br /> insights have significantly improved our business
@@ -25,12 +25,12 @@ const Testimonials = () => {
             </p>
           </div>
         </div>
-        <div className="flex">
+        <div className="flex gap-[800px]">
             <div className="font-inter">
                 <p className="text-[20px]">Floyd Miles</p>
                 <p className="text-base opacity-80">The Walt Disney Company</p>
             </div>
-            <div className="flex">
+            <div className="flex gap-4 items-center">
                 <button><img src="/Testimonial/ArrowLeft.svg" alt="prev" /></button>
                 <button><img src="/Testimonial/ArrowRight.svg" alt="next" /></button>
             </div>
