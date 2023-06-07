e245c6782f52df88f5842b1bbafc19925e34cc39
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-06-07 16:54:08 +0300

    Created the othjer services section

--- client/src/components/Servicesextension.tsx
+++ client/src/components/Servicesextension.tsx
@@ -1,9 +1,16 @@
+import OtherServicescards from "./OtherServicescards";
+
 const Servicesextension = () => {
   return (
     <>
-      
+      <div className="pt-52">
+        <div className="text-center pb-10">
+          <h3 className="font-archivo text-customBlue text-[58px] font-semibold">Other Services</h3>
+        </div>
+        <OtherServicescards />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Servicesextension
+export default Servicesextension;
