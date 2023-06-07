a0cf1a71adf6b901fffabafb388e2efb0608a90a
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-06-07 17:08:50 +0300

    Added the tesimonials section

--- client/src/components/Cta.tsx
+++ client/src/components/Cta.tsx
@@ -1,12 +1,13 @@
+import Testimonials from "./Testimonials";
+
 const Cta = () => {
   return (
     <>
-    <div>
-        
-    </div>
-      
+      <div className="bg-customBlue rounded-t-[100px] relative">
+        <Testimonials />
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default Cta;
---
+++ client/src/components/Testimonials.tsx
@@ -0,0 +1,12 @@
+
+const Testimonials = () => {
+  return (
+    <>
+      <div>
+        
+      </div>
+    </>
+  )
+}
+
+export default Testimonials
