b546202f6b7ca05fe6740bf269c6d8eb0e64c285
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-06-09 00:44:22 +0300

    Created the Footer section and styled it

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -20,7 +20,46 @@ const Footer = () => {
           </div>
         </div>
         <div className="pt-5 pb-5">
-            <hr className="bg-white opacity-30 ml-[280px] mr-[360px]" />
+          <hr className="bg-white opacity-30 ml-[280px] mr-[360px] border-[1px]" />
+        </div>
+        <div className="flex ml-[280px] gap-[920px] ">
+          <div className="flex-col">
+            <p className="font-inter text-white font-semibold py-5">Location</p>
+            <div className="font-inter opacity-50 text-white">
+              <p className="pb-2">
+                Limarge homes <br /> bulding, ground floor, <br /> Nairobi Meru
+                highway, <br /> opposite Kannet <br /> enterprises.
+              </p>
+              <p className="pb-2">mailto:janeefery@gmail.com</p>
+              <p className="pb-2">P.o box 2564 Embu</p>
+              <p className="pb-2">Tel + 254 707 927 167</p>
+            </div>
+          </div>
+          <div className="flex-col text-white">
+            <p className="font-inter text-white font-semibold py-5">
+              Quick Links
+            </p>
+            <div className="font-inter opacity-50">
+              <p className="pb-2">About</p>
+              <p className="pb-2">Services</p>
+              <p className="pb-2">Contact</p>
+              <p className="pb-2">Home</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex font-inter opacity-50 text-white gap-[500px] pt-7">
+          <div className="ml-[280px]">
+            <p>Copyright @ LIMARGE CONSULTANTS 2023. All rights reserved.</p>
+          </div>
+          <div className="flex">
+            <div className="pr-5">
+              <p>Terms and service</p>
+            </div>
+            <div className="pl-1">
+              <p>Privacy policy</p>
+            </div>
+          </div>
         </div>
       </div>
     </>
