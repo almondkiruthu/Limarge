a5e91c1ecccc698eed6ec19597bc30c00cd97d99
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-06-08 23:52:58 +0300

    Added the footer assets

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,7 +1,15 @@
 const Footer = () => {
   return (
     <>
-      <div className="footer"></div>
+      <div className="footer">
+
+        <div>
+            <div>
+                <
+            </div>
+        </div>
+
+      </div>
     </>
   );
 };
