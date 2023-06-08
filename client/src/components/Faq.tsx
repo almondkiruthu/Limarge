292d44353759631a7b93ba59e769d7b95dd7ae7c
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-06-09 00:19:18 +0300

    Created the upper part of the footer

--- client/src/components/Faq.tsx
+++ client/src/components/Faq.tsx
@@ -4,7 +4,7 @@ import Accordion from "./Accordion";
 const Faq = () => {
   return (
     <>
-      <div className="bg-customBlue text-white pt-44">
+      <div className="bg-customBlue text-white pt-44 pb-20">
         <div className="flex  gap-[40px]">
           <div className="flex-col ml-[300px]">
             <h3 className="font-archivo text-[58px] font-semibold">
--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,14 +1,27 @@
 const Footer = () => {
   return (
     <>
-      <div className="footer">
-
-        <div>
+      <div className="footer pt-12">
+        <div className="flex items-center gap-[800px] ml-[270px]">
+          <div>
+            <img src="/Footer/Logo.svg" alt="logo" />
+          </div>
+          <div className="flex">
             <div>
-                <
+              <p className="font-inter text-base opacity-70 text-white pt-3 pr-4">
+                Send us email at
+              </p>
             </div>
+            <div className="font-inter text-center ml-2">
+              <button className="contact-us-btn px-8 py-3 rounded-[12px] font-medium text-white text-center">
+                limarge@example.com
+              </button>
+            </div>
+          </div>
         </div>
-
+        <div className="pt-5 pb-5">
+            <hr className="bg-white opacity-30 ml-[280px] mr-[360px]" />
+        </div>
       </div>
     </>
   );
