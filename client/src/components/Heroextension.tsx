d0aba836a3f2032ca45f065cbd8e171de91e14e0
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-06-11 23:28:04 +0300

    Made the hero extension responsive

--- client/src/components/Heroextension.tsx
+++ client/src/components/Heroextension.tsx
@@ -1,18 +1,18 @@
 const Heroextension = () => {
   return (
     <>
-      <div className="flex-row-reverse bg-customBlue rounded-t-[40px] lg:flex pb-32">
+      <div className="flex-row-reverse bg-customBlue rounded-t-[40px] lg:flex pb-[60px]">
         
         <div className="">
-          <div className="px-[30px] lg:ml-[250px]">
+          <div className="px-[30px] lg:mr-[200px]">
             <img src="./Heroextension/UserBg.svg" alt="ClientsBackground" />
           </div>
-          <div className="absolute bottom-[30px]  right-[50px] lg:bottom-[-120px] lg:right-[280px]">
-            <img src="./Heroextension/User.svg" alt="HappyClient" className="h-[250px]" />
+          <div className="absolute bottom-[30px]  right-[50px] lg:bottom-[120px] lg:right-[240px]">
+            <img src="./Heroextension/User.svg" alt="HappyClient" className="h-[250px] lg:h-[420px]" />
           </div>
         </div>
-        <div className=" pt-5 lg:flex lg:items-baseline lg:pl-[218px] lg:pt-10 pb-5 lg:gap-12">
-          <div className="flex justify-center">
+        <div className=" pt-10 lg:flex lg:items-baseline lg:pl-[218px] lg:pt-10 pb-5 lg:gap-x-12 lg:mr-[256px]">
+          <div className="flex justify-center gap-x-[40px]">
             <div>
               <img src="./Heroextension/AlphabetLogo.svg" alt="AlphabetLogo" />
             </div>
@@ -21,7 +21,7 @@ const Heroextension = () => {
             </div>
           </div>
 
-          <div className="flex justify-center">
+          <div className="flex justify-center gap-x-[40px]">
             <div>
               <img src="./Heroextension/AzureLogo.svg" alt="azurelogo" />
             </div>
