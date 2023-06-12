2c0c220de3d7d8675106a6f9f29b86e2165f0b06
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-06-12 15:33:40 +0300

    Made the CTA section fully responsive

--- client/src/components/Cta.tsx
+++ client/src/components/Cta.tsx
@@ -11,13 +11,13 @@ const Cta = () => {
         <h2 className="font-archivo text-[40px] lg:text-[58px] font-semibold">
           Have an Idea? Let's <br /> consult and scale it <br /> together
         </h2>
-        <div className="font-inter pt-24 text-center">
-          <button className="contact-us-btn px-8 py-3 rounded-[12px] font-medium text-white text-center">
+        <div className="font-inter pt-[50px] lg:pt-20 text-center px-4">
+          <button className="contact-us-btn w-[100%] lg:w-[10%] lg:px-8 py-3 rounded-[12px] font-medium text-white text-center">
             Book a demo
           </button>
         </div>
-        <div className="absolute top-[200px]">
-          <img src="/Cta/Mask.svg" alt="Line" />
+        <div className="invisible lg:visible lg:absolute lg:top-[200px]">
+          <img src="/Cta/Mask.svg" alt="Line" className="h-[0] lg:h-[100%]" />
         </div>
       </div>
       <Faq />
