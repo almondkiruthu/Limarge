b53166b6b2e19d7d60337e77f02568a54e981fa4
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-06-12 14:49:30 +0300

    Made the ServicesExtension and the OtherServicescards reponsive

--- client/src/components/Servicesextension.tsx
+++ client/src/components/Servicesextension.tsx
@@ -3,9 +3,9 @@ import OtherServicescards from "./OtherServicescards";
 const Servicesextension = () => {
   return (
     <>
-      <div className="pt-52">
+      <div className="pt-20 lg:pt-52">
         <div className="text-center pb-10">
-          <h3 className="font-archivo text-customBlue text-[58px] font-semibold">Other Services</h3>
+          <h3 className="font-archivo text-customBlue text-[40px] lg:text-[58px] font-semibold">Other Services</h3>
         </div>
         <OtherServicescards />
       </div>
