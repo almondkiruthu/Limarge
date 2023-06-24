256ac16f71195dfc0b749229cf83939d78aae906
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-06-24 12:13:58 +0300

    Added the testimonial data

--- client/lib/testimonialData.js
+++ client/lib/testimonialData.js
@@ -1,1 +1,25 @@
-const 
+const testimonialData = [
+  {
+    id: 1,
+    clientName: "Director-Catherine Ndugu",
+    companyName: "Etels Institute",
+    message:
+      "We at Etels Institute of Professionals Ltd take this opportunity to express our appreciation for the training offered by your firm to our accounts staff on Financial Management in our offices at Embu. We are in receipt of the report that will help us monitor the efficiency of the accounts staff in relation to keeping proper and accurate books of accounts We look forward to working with you in future.",
+  },
+  {
+    id: 2,
+    clientName: "Program Manager-Fredrick Kyalo.S",
+    companyName: "Fair Trade Organization of Kenya",
+    message:
+      "The Fair Trade Organization of Kenya (FTOK) take this opportunity to express sincere appreciation for the training offered by your firm to the Managers and assistant Managers of Gakundu Farmers Co-operative Society (FCS) on Capacity Building on Basic Accounting at their Gichugu Coffee factory. We are in receipt of the report that will help us monitor their productivity in relation to keeping proper and accurate books of accounts.",
+  },
+  {
+    id: 3,
+    clientName: "HOD-Accounts- Mrs.E.Kiragu",
+    companyName: "Embu College",
+    message:
+      "We take this opportunity to express our gratitude for accepting our invitation to train our CPA lecturers in Performance Management. It is our hope that the results of the workshop will be reflected in the results of the forthcoming examination and also for the attainment of the vision of the college We look forward to working with you in future.",
+  },
+];
+
+export default testimonialData;
