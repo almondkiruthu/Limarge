2b16c14862097fba7ca6efea2e2b13e408b4dd8e
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-06-24 12:47:39 +0300

    Created the testimonials logic

--- client/lib/Context.tsx
+++ client/lib/Context.tsx
@@ -1,21 +1,38 @@
 import { createContext, useContext, useState } from "react";
-
+// @ts-ignore
+import testimonialData from "../lib/testimonialData.js";
 
 //@ts-ignore
 const TestimonialContext = createContext();
 
-
 // @ts-ignore
 export const ContextProvider = ({ children }) => {
-  const [testimonials, setTestimonials] = useState([]);
+  const [testimonials, setTestimonials] = useState([testimonialData[0]]);
+  const [currentTestimonialIndex, setCurrentTestimonialIndex] = useState(0);
 
+  // @ts-ignore
+  const handlePrevTestimonial = () => {
+    const newIndex =
+      (currentTestimonialIndex - 1 + testimonials.length) % testimonials.length;
+
+    setCurrentTestimonialIndex(newIndex);
+  };
+
+  // @ts-ignore
+  const handleNextTestimonial = () => {
+    const newIndex = (currentTestimonialIndex + 1) % testimonials.length;
+
+    setCurrentTestimonialIndex(newIndex);
+  };
+
+  // @ts-ignore
+  const currentTestimonial = testimonials[currentTestimonialIndex];
 
   return (
-    <TestimonialContext.Provider value={{ testimonials, setTestimonials }}>
+    <TestimonialContext.Provider value={{ testimonials, setTestimonials, handlePrevTestimonial, handleNextTestimonial, currentTestimonial}}>
       {children}
     </TestimonialContext.Provider>
   );
 };
 
 export const useTestimonialContext = () => useContext(TestimonialContext);
-
