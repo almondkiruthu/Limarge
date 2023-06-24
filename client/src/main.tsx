e4e697c2e03498d779552f613d5f16eda4e0d4ba
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-06-24 20:44:24 +0300

    change file extension in context

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { ContextProvider } from "../lib/context";
+import { ContextProvider } from "../lib/context.jsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
