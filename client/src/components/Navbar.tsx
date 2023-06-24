84791344aa65e3b20fdba06bb354f5610f750112
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-06-24 17:23:04 +0300

    Changed the navbar shadow abit

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -37,7 +37,7 @@ export default function NavBar() {
   const [navbar, setNavbar] = useState(false);
 
   return (
-    <nav className="w-full py-5 shadow-md">
+    <nav className="w-full py-5 shadow">
       <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
         <div>
           <div className="flex items-center justify-between py-3 md:py-5 md:block">
