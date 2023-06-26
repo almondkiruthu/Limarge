33c5527517bc64526087f66f13e852702c043fb9
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-06-26 18:33:06 +0300

    Add animation to the hero image

--- client/src/components/Heroextension.tsx
+++ client/src/components/Heroextension.tsx
@@ -13,9 +13,9 @@ const Heroextension = () => {
             />
           </div>
           <motion.div
-            initial={{ opacity: 0 }}
-            whileInView={{ opacity: 1 }}
-            transition={{ delay: 1 }}
+            initial={{ opacity: 0, y: 100 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ delay: 1, duration: 0.5 }}
             className="absolute top-[-142px] inset-0 object-cover lg:top-[-220px] lg:right-[13%]"
           >
             <img
