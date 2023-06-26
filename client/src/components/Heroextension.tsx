ce4a1e5492db0d57d1bfc0db892dd883dca405b3
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-06-26 18:11:45 +0300

    Import of the Framer motion in the hero extension component

--- client/src/components/Heroextension.tsx
+++ client/src/components/Heroextension.tsx
@@ -1,15 +1,24 @@
+import { motion } from "framer-motion";
+
 const Heroextension = () => {
   return (
     <>
       <div className="flex-row-reverse bg-customBlue rounded-t-[40px] lg:flex pb-[60px]">
-        
         <div className="user-bg relative">
           <div className="px-[50px] lg:mr-[200px] -z-10">
-            <img src="./Heroextension/UserBg.svg" alt="ClientsBackground" className="h-[105.311px] w-[296.01px] lg:h-[100%] lg:w-[100%]"/>
+            <img
+              src="./Heroextension/UserBg.svg"
+              alt="ClientsBackground"
+              className="h-[105.311px] w-[296.01px] lg:h-[100%] lg:w-[100%]"
+            />
           </div>
-          <div className="absolute top-[-142px] inset-0 object-cover lg:top-[-220px] lg:right-[13%]">
-            <img src="./Heroextension/User.svg" alt="HappyClient" className="h-full px-9" />
-          </div>
+          <motion.div className="absolute top-[-142px] inset-0 object-cover lg:top-[-220px] lg:right-[13%]">
+            <img
+              src="./Heroextension/User.svg"
+              alt="HappyClient"
+              className="h-full px-9"
+            />
+          </motion.div>
         </div>
         <div className=" pt-10 lg:flex lg:items-baseline lg:pl-[218px] lg:pt-10 pb-5 lg:gap-x-12 lg:mr-[256px]">
           <div className="flex justify-center gap-x-[40px]">
