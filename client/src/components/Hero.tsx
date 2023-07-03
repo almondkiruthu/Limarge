dc929d616083dee9b15ab46e0c1c689d354b164f
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-07-03 17:12:45 +0300

    Makin the hero responsive

--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -3,56 +3,53 @@ import Heroextension from "./Heroextension";
 const Hero = () => {
   return (
     <>
-      <div className="lg:ml-56 lg:mt-6 relative lg:pb-10 pb-32">
-        <div className="lg:items-baseline font-extrabold text-customBlue">
-          <div className="lg:flex pl-4 flex-wrap">
-            <div className="flex">
-              <h1 className="font-archivo text-[42px] pr-4  lg:text-[98px]">
-                We
-              </h1>
-              <h1 className="font-archivo text-[42px] lg:text-[98px]">
-                elevate
+      <div className="lg:pl-32 lg:pt-6 relative lg:pb-10 pb-32">
+        <div className="font-extrabold text-customBlue">
+          <div className="flex justify-start items-baseline pl-4 flex-wrap">
+            <div className="">
+              <h1 className="font-archivo text-[42px] pr-4  lg:text-[6.125rem]">
+                We elevate
               </h1>
             </div>
-            <div className="flex items-baseline">
-              <h1 className="font-archivo text-[42px] pr-[5px] lg:text-[98px] lg:pl-4">
-                and
-              </h1>
-              <div className="lg:px-3">
-                <img
-                  src="./Hero/HeroTextImage1.svg"
-                  alt="HeroTextImage1"
-                  className="w-[40px] lg:w-[80px]"
-                />
-              </div>
-              <h1 className="font-archivo text-[42px] lg:text-[98px] pl-[5px]">
-                enrich
-              </h1>
+            <h1 className="font-archivo text-[42px] lg:text-[6.125rem] ">
+              and
+            </h1>
+            <div className="lg:px-3">
+              <img
+                src="./Hero/HeroTextImage1.svg"
+                alt="HeroTextImage1"
+                className="w-[2.5rem] lg:w-[100%]"
+              />
             </div>
+            <h1 className="font-archivo text-[42px] lg:text-[6.125rem]">
+              enrich
+            </h1>
           </div>
         </div>
-        <div className="lg:items-baseline font-extrabold text-customBlue">
-          <div className="lg:flex pl-4">
-            <div className="flex items-baseline">
-              <img
-                src="./Hero/HeroTextImage2.svg"
-                alt="HeroTextImage2"
-                className="w-[60px] lg:w-[120px]"
-              />
-              <h1 className="font-archivo text-[42px] lg:text-[98px] pr-4">
-                change
+        <div className="font-extrabold text-customBlue">
+          <div className="pl-4">
+            <div className="flex items-baseline flex-wrap">
+              <div>
+                <img
+                  src="./Hero/HeroTextImage2.svg"
+                  alt="HeroTextImage2"
+                  className="hidden lg:block"
+                />
+              </div>
+              <h1 className="font-archivo text-[42px] lg:text-[6.125rem] pr-4">
+                change across
               </h1>
-            </div>
-            <div className="flex">
-              <h1 className="font-archivo text-[42px] pr-[4px] lg:text-[98px]">
+              {/* <h1 className="font-archivo text-[42px] lg:text-[98px] pr-4">
                 across
-              </h1>
-              <h1 className="font-archivo text-[42px] lg:text-[98px] pl-[5px]">
+              </h1> */}
+              <h1 className="font-archivo text-[42px] lg:text-[6.125rem] pr-4">
                 industries
               </h1>
             </div>
           </div>
         </div>
+
+        {/* Hero p text */}
 
         <div className="pt-5 pl-4">
           <p className="font-inter text-customBlue  text-[18px] lg:text-xl">
@@ -60,16 +57,21 @@ const Hero = () => {
             consultancy at your <br /> service.
           </p>
         </div>
+
+        {/* hero buttton */}
+
         <div className="font-inter pt-9 px-2">
-          <button className="contact-us-btn w-full lg:w-[10%] px-8 py-3 rounded-[12px] font-medium text-white text-center">
+          <button className="contact-us-btn w-full lg:max-w-[10rem] px-8 py-3 rounded-[12px] font-medium text-white text-center">
             Contact us
           </button>
         </div>
-        <div className="absolute right-[-30px] bottom-[-5px] lg:top-[100px] overflow-x-hidden">
+
+        {/* Hero vibrant ribbon  */}
+        <div className="absolute right-[-30px] bottom-[-5px] lg:top-[100px] ">
           <img
             src="./Hero/VibrantHeroLine.svg"
             alt="line"
-            className="h-[150px] lg:h-[80%]"
+            className="w-[150px] lg:w-[100%]"
           />
         </div>
       </div>
