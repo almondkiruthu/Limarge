7fb31da39d41b35bd0e06eb245533adcb2cda607
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-07-03 16:23:13 +0300

    Refactor navbar code

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,36 +1,3 @@
-// const Navbar = () => {
-//   return (
-//     <>
-//       <div>
-//         <header className="bg-customBackground">
-//           <nav className="flex justify-around py-5 items-center">
-//             <div className="flex">
-//               <div className="mr-2">
-//                 <img src="./Navbar/Logo.svg" alt="logo" />
-
-//               <p className="text-xl font-semibold font-inter">LIMARGE</p>
-//               </div>
-//             </div>
-//             <div className="absolute top-[-90px]  lg:relative lg:top-0">
-//               <ul className="font-inter text-customBlue flex items-center gap-[80px]">
-//                 <li>Home</li>
-//                 <li>Services</li>
-//                 <li>Stories</li>
-//                 <li>About Us</li>
-//                 <li>Company</li>
-//               </ul>
-//             </div>
-//             <div className="font-inter text-customBlue">
-//               <button className="p-2 border-customOrange border-2 rounded-lg">Contact us</button>
-//             </div>
-//           </nav>
-//         </header>
-//       </div>
-//     </>
-//   );
-// };
-
-// export default Navbar;
 import { useState } from "react";
 
 export default function NavBar() {
