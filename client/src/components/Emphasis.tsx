873b5fa7292fc84784719297dbe6b9bda36846eb
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-07-11 23:46:06 +0300

    Set padding left to zero for empahsis text on 2xl screens

--- client/src/components/Emphasis.tsx
+++ client/src/components/Emphasis.tsx
@@ -3,25 +3,25 @@ import { motion } from "framer-motion";
 const Emphasis = () => {
   return (
     <>
-      <div className="bg-customBlue 2xl:pl-56 text-white flex flex-wrap 2xl:pt-10 pb-12">
+      <div className="flex flex-wrap bg-customBlue pb-12 text-white 2xl:pl-56 2xl:pt-10">
         <div>
           <div>
-            <h2 className="font-archivo font-semibold text-[40px] pl-4 lg:pl-0 2xl:text-[58px] ">
+            <h2 className="pl-4 font-archivo text-[40px] font-semibold 2xl:pl-0 2xl:text-[58px] ">
               Unlocking <br /> Potential. Driving <br /> Success.
             </h2>
           </div>
           <div className="pt-4">
-            <p className="font-inter text-base pl-4 lg:pl-0">
+            <p className="pl-4 font-inter text-base lg:pl-0">
               Experience transformative consultancy solutions <br /> tailored to
               your unique industry needs.
             </p>
           </div>
-          <div className="font-inter pt-8 pl-3 lg:pl-0">
-            <button className="contact-us-btn w-[100%] lg:w-[40%] px-8 py-3 rounded-[12px] font-medium text-white text-center">
+          <div className="pl-3 pt-8 font-inter lg:pl-0">
+            <button className="contact-us-btn w-[100%] rounded-[12px] px-8 py-3 text-center font-medium text-white lg:w-[40%]">
               Contact us
             </button>
           </div>
-          <div className="pt-5 pl-4 lg:pl-0">
+          <div className="pl-4 pt-5 lg:pl-0">
             <div className="">
               <img
                 src="./EmphasisHeroSection/Clients.svg"
@@ -29,14 +29,14 @@ const Emphasis = () => {
                 className="h-[50px]"
               />
             </div>
-            <p className="font-inter font-semibold text-[28px] pt-5">10,000+</p>
-            <p className="font-inter font-regular text-xl pt-2 opacity-70">
+            <p className="pt-5 font-inter text-[28px] font-semibold">10,000+</p>
+            <p className="font-regular pt-2 font-inter text-xl opacity-70">
               Positive feedback
             </p>
           </div>
         </div>
         <div className=" lg:ml-[250px]">
-          <motion.div className="pt-4 pl-5 lg:pl-0">
+          <motion.div className="pl-5 pt-4 lg:pl-0">
             <motion.img
               initial={{ opacity: 0 }}
               whileInView={{ opacity: 1 }}
