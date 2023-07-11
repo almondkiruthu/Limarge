29ac3abf103c1524c66b0c9cf9d4de55398fc860
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-07-11 23:51:49 +0300

    Refactor code for the emphasis component

--- client/src/components/Emphasis.tsx
+++ client/src/components/Emphasis.tsx
@@ -6,7 +6,7 @@ const Emphasis = () => {
       <div className="flex flex-wrap bg-customBlue pb-12 text-white 2xl:pl-56 2xl:pt-10">
         <div>
           <div>
-            <h2 className="pl-4 font-archivo text-[40px] font-semibold 2xl:pl-0 2xl:text-[58px] xl:text-[3rem] ">
+            <h2 className="pl-4 font-archivo text-[40px] font-semibold xl:text-[3rem] 2xl:pl-0 2xl:text-[58px] ">
               Unlocking <br /> Potential. Driving <br /> Success.
             </h2>
           </div>
