01498d4b1f0f7c42f0fe18d4ed0526bceb470b9f
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-07-13 19:14:10 +0300

    replace 25px with 1.75rem

--- client/src/components/Servicecards.tsx
+++ client/src/components/Servicecards.tsx
@@ -65,7 +65,7 @@ const Servicecards = () => {
         {/* Business card */}
 
         <div className="business-card pt-5 3xl:pt-10 pr-5 3xl:pr-10 pl-5 3xl:pl-10  pb-2 3xl:pb-5 rounded-[1.25rem] mx-auto">
-          <p className="font-archivo text-[28px] font-semibold pb-3">
+          <p className="font-archivo text-[1.75rem] font-semibold pb-3">
             Business and <br /> Market Research
           </p>
           <div>
