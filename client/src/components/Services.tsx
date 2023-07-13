425f3fba4eae33a8e64bf8efe73e12d2fc1f1a0d
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-07-13 19:57:28 +0300

    Services COmponent  responsive on 2xl screens

--- client/src/components/Services.tsx
+++ client/src/components/Services.tsx
@@ -5,20 +5,20 @@ const Services = () => {
     <>
       <div className="relative bg-customBlue pt-20 text-white">
         <div>
-          <h2 className="pb-14 text-center font-archivo text-[2.5rem] font-semibold 3xl:text-[3.625rem]">
+          <h2 className="pb-14 text-center font-archivo text-[2.5rem] font-semibold 2xl:text-[3rem] 3xl:text-[3.625rem]">
             Our Main <br /> Services
           </h2>
         </div>
         <Servicecards />
-        <div className="invisble 3xl:visible 3xl:absolute 3xl:right-[18%] 3xl:top-4">
+        <div className="invisble 2xl:visible 2xl:absolute 2xl:right-[10%] 2xl:top-4 3xl:visible 3xl:absolute 3xl:right-[18%] 3xl:top-4">
           <img
             src="/Servicesassets/Star.svg"
             alt="star"
-            className="h-0 3xl:h-[100%]"
+            className="h-0 2xl:h-full 3xl:h-full"
           />
         </div>
         <div className="px-4 pt-[3.125rem] text-center font-inter 3xl:pt-20">
-          <button className="contact-us-btn w-[100%] rounded-[0.75rem] py-3 text-center font-medium text-white 3xl:w-[10%] 3xl:px-8">
+          <button className="contact-us-btn w-[100%] rounded-[0.75rem] py-3 text-center font-medium text-white 2xl:max-w-[10%] 3xl:w-[10%] 3xl:px-8">
             Contact us
           </button>
         </div>
