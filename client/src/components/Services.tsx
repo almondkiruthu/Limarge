a8baf979608991b7d3849f6e04f148aea6f57450
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-07-13 19:28:11 +0300

    Changed all the large breakpoints in Services to 3xl breakpoints

--- client/src/components/Services.tsx
+++ client/src/components/Services.tsx
@@ -5,16 +5,16 @@ const Services = () => {
     <>
       <div className="bg-customBlue text-white pt-20 relative">
         <div>
-          <h2 className="font-archivo text-[2.5rem] lg:text-[3.625rem] text-center font-semibold pb-14">
+          <h2 className="font-archivo text-[2.5rem] 3xl:text-[3.625rem] text-center font-semibold pb-14">
             Our Main <br /> Services
           </h2>
         </div>
         <Servicecards />
-        <div className="invisble lg:visible lg:absolute lg:right-[18%] lg:top-4">
-          <img src="/Servicesassets/Star.svg" alt="star" className="h-0 lg:h-[100%]" />
+        <div className="invisble 3xl:visible 3xl:absolute 3xl:right-[18%] 3xl:top-4">
+          <img src="/Servicesassets/Star.svg" alt="star" className="h-0 3xl:h-[100%]" />
         </div>
-        <div className="font-inter pt-[3.125rem] lg:pt-20 text-center px-4">
-          <button className="contact-us-btn w-[100%] lg:w-[10%] lg:px-8 py-3 rounded-[0.75rem] font-medium text-white text-center">
+        <div className="font-inter pt-[3.125rem] 3xl:pt-20 text-center px-4">
+          <button className="contact-us-btn w-[100%] 3xl:w-[10%] 3xl:px-8 py-3 rounded-[0.75rem] font-medium text-white text-center">
             Contact us
           </button>
         </div>
