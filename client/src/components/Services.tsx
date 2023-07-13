1e9ff2c5f42db49eb8be19a37c535affa4e783a1
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-07-13 19:30:31 +0300

    Refactor the code in Services component

--- client/src/components/Services.tsx
+++ client/src/components/Services.tsx
@@ -3,18 +3,22 @@ import Servicecards from "./Servicecards";
 const Services = () => {
   return (
     <>
-      <div className="bg-customBlue text-white pt-20 relative">
+      <div className="relative bg-customBlue pt-20 text-white">
         <div>
-          <h2 className="font-archivo text-[2.5rem] 3xl:text-[3.625rem] text-center font-semibold pb-14">
+          <h2 className="pb-14 text-center font-archivo text-[2.5rem] font-semibold 3xl:text-[3.625rem]">
             Our Main <br /> Services
           </h2>
         </div>
         <Servicecards />
         <div className="invisble 3xl:visible 3xl:absolute 3xl:right-[18%] 3xl:top-4">
-          <img src="/Servicesassets/Star.svg" alt="star" className="h-0 3xl:h-[100%]" />
+          <img
+            src="/Servicesassets/Star.svg"
+            alt="star"
+            className="h-0 3xl:h-[100%]"
+          />
         </div>
-        <div className="font-inter pt-[3.125rem] 3xl:pt-20 text-center px-4">
-          <button className="contact-us-btn w-[100%] 3xl:w-[10%] 3xl:px-8 py-3 rounded-[0.75rem] font-medium text-white text-center">
+        <div className="px-4 pt-[3.125rem] text-center font-inter 3xl:pt-20">
+          <button className="contact-us-btn w-[100%] rounded-[0.75rem] py-3 text-center font-medium text-white 3xl:w-[10%] 3xl:px-8">
             Contact us
           </button>
         </div>
