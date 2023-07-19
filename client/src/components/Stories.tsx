000f40a1a8e44ac84c93887baa45f0824561c630
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-07-19 23:31:53 +0300

    Change gap to 4 in 2xl screens

--- client/src/components/Stories.tsx
+++ client/src/components/Stories.tsx
@@ -16,7 +16,7 @@ const Stories = () => {
             className="h-[0] 3xl:h-[100%]"
           />
         </div>
-        <div className="flex flex-wrap gap-y-10 pt-10 3xl:ml-[3.125rem] 3xl:gap-x-4  3xl:pl-0 3xl:pt-24">
+        <div className="flex flex-wrap gap-y-10 pt-10 2xl:gap-x-3 3xl:ml-[3.125rem] 3xl:gap-x-4  3xl:pl-0 3xl:pt-24">
           <div className="mx-auto rounded-[12px] bg-white">
             <div className=" px-[60px] py-4  text-customBlue 3xl:px-[75px] 3xl:py-8">
               <div>
