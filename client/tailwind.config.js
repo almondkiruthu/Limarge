768b983d6a9cfb4fdb602ce4720297b0698bb21f
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-07-24 21:54:10 +0300

    Added the container configurations in my tailwind config file

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,6 +2,10 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    container: {
+      center: true,
+      padding: "0.5rem",
+    },
     extend: {
       fontFamily: {
         archivo: ["Archivo", "sans-serif"],
