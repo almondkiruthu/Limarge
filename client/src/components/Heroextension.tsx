e29efce42e81e97801a5ad494ae47f01f907bde7
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-07-25 18:22:31 +0300

    Fixed the hero extension on 2xl screens and above

--- client/src/components/Heroextension.tsx
+++ client/src/components/Heroextension.tsx
@@ -4,48 +4,33 @@ const Heroextension = () => {
   return (
     <>
       <div className="rounded-t-[2.5rem] bg-customBlue pb-[3.75rem]">
-        <div className="hero__extension__container container lg:flex-row-reverse 2xl:flex">
-          <div className="user-bg relative mx-auto xl:ml-[8rem]">
-            <div className="-z-10 px-[15%] xl:px-0 xl:pl-[0rem] 2xl:ml-auto 2xl:pl-0">
+        <div className="hero__extension__container container lg:flex 2xl:flex 2xl:items-center">
+          <div className=" 2xl:m pb-5 pt-10 2xl:ml-[5rem] 2xl:mr-auto 2xl:flex 2xl:gap-x-[1.5rem]">
+            <img src="./Heroextension/AlphabetLogo.svg" alt="AlphabetLogo" />
+            <img src="./Heroextension/AmazonLogo.svg" alt="amazonlogo" />
+            <img src="./Heroextension/AzureLogo.svg" alt="azurelogo" />
+            <img src="./Heroextension/SlackLogo.svg" alt="slacklogo" />
+          </div>
+          <div className="relative 2xl:ml-auto 2xl:mr-auto 2xl:flex-col 2xl:items-center 2xl:justify-center">
+            <div className="user__background">
               <img
                 src="./Heroextension/UserBg.svg"
                 alt="ClientsBackground"
-                className="h-full w-full xl:h-[100%] xl:w-[80%] 2xl:w-[100%]"
+                className=""
               />
             </div>
+
             <div
               // initial={{ opacity: 0, y: 100 }}
               // whileInView={{ opacity: 1, y: 0 }}
               // transition={{ delay: 0.8, duration: 0.5 }}
-              className="absolute inset-0 top-[-142px] object-cover 2xl:right-[1%] 2xl:top-[-220px]"
+              className=""
             >
               <img
                 src="./Heroextension/User.svg"
                 alt="HappyClient"
-                className="h-full object-cover px-[10%] xl:px-0"
+                className="absolute 2xl:top-[-13.75rem]"
               />
-            </div>
-          </div>
-          <div className=" pb-5 pt-10 xl:flex xl:pl-24 2xl:mr-auto 2xl:flex 2xl:items-baseline 2xl:gap-x-12 2xl:pl-[8rem] 2xl:pt-10">
-            <div className="flex justify-center gap-x-[2.5rem]">
-              <div>
-                <img
-                  src="./Heroextension/AlphabetLogo.svg"
-                  alt="AlphabetLogo"
-                />
-              </div>
-              <div>
-                <img src="./Heroextension/AmazonLogo.svg" alt="amazonlogo" />
-              </div>
-            </div>
-
-            <div className="flex justify-center gap-x-[2.5rem] xl:ml-[2rem] 2xl:ml-0">
-              <div>
-                <img src="./Heroextension/AzureLogo.svg" alt="azurelogo" />
-              </div>
-              <div>
-                <img src="./Heroextension/SlackLogo.svg" alt="slacklogo" />
-              </div>
             </div>
           </div>
         </div>
