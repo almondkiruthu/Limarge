42f5dc9e6e1164b91d5874a027f1750b3c78030b
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-07-28 21:07:41 +0300

    Set the hero extended image and the Background to 90% width on xl screens and above

--- client/src/components/Heroextension.tsx
+++ client/src/components/Heroextension.tsx
@@ -16,7 +16,7 @@ const Heroextension = () => {
               <img
                 src="./Heroextension/UserBg.svg"
                 alt="ClientsBackground"
-                className=""
+                className="xl:w-[90%]"
               />
             </div>
 
@@ -29,7 +29,7 @@ const Heroextension = () => {
               <img
                 src="./Heroextension/User.svg"
                 alt="HappyClient"
-                className="absolute xl:top-[-13.73rem] 2xl:top-[-13.73rem]"
+                className="absolute xl:w-[90%] xl:top-[-13.73rem] 2xl:w-[100%] 2xl:top-[-13.73rem]"
               />
             </div>
           </div>
