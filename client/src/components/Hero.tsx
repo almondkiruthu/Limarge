c7aa08fde458446dd8ddb8058c4a666b17014f8e
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-07-29 17:24:23 +0300

    I made the entire hero responsive on large screens and above

--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -3,9 +3,9 @@ import Heroextension from "./Heroextension";
 const Hero = () => {
   return (
     <>
-      <div className="container pb-32 lg:pt-[1rem] xl:pb-[4rem] 2xl:pb-[3rem]">
+      <div className="container pb-32  lg:pb-[4rem] lg:pt-[1rem] 2xl:pb-[3rem]">
         <div className="font-extrabold text-customBlue">
-          <div className="flex flex-wrap items-baseline justify-start pl-4 xl:mr-[11rem] lg:justify-center 2xl:mr-[12rem] 2xl:justify-center 2xl:pl-0">
+          <div className="flex flex-wrap items-baseline justify-start pl-4 lg:mr-[9rem] lg:justify-center xl:mr-[11rem] 2xl:mr-[12rem] 2xl:justify-center 2xl:pl-0">
             <div className="">
               <h1 className=" pr-4 font-archivo text-[2.625rem] lg:text-[4.125rem] xl:text-[5.125rem] 2xl:text-[6.125rem]">
                 We elevate and
@@ -25,12 +25,12 @@ const Hero = () => {
         </div>
         <div className="font-extrabold text-customBlue">
           <div className="pl-4 2xl:pl-0">
-            <div className="flex flex-wrap items-baseline xl:justify-center 2xl:justify-center">
+            <div className="flex flex-wrap items-baseline lg:justify-center 2xl:justify-center">
               <div>
                 <img
                   src="./Hero/HeroTextImage2.svg"
                   alt="HeroTextImage2"
-                  className="lg:w-[5.43606rem] xl:w-[6.43606rem] 2xl:w-[6.43606rem]"
+                  className="lg:w-[5.33606rem] xl:w-[6.43606rem] 2xl:w-[6.43606rem]"
                 />
               </div>
               <h1 className="pr-4 font-archivo text-[2.625rem] lg:text-[4.125rem] xl:text-[5.125rem] 2xl:text-[6.125rem]">
@@ -45,7 +45,7 @@ const Hero = () => {
 
         {/* Hero p text */}
 
-        <div className="pl-4 pt-5 text-customBlue lg:flex xl:ml-[3.5rem] 2xl:ml-[5.5rem] 2xl:pl-0 2xl:pt-[2rem]">
+        <div className="pl-4 pt-5 text-customBlue lg:ml-[2rem] lg:flex xl:ml-[3.5rem] 2xl:ml-[5.5rem] 2xl:pl-0 2xl:pt-[2rem]">
           <p className="font-inter text-[1.125rem]  2xl:text-[1.25rem]">
             Unleash growth potential. Empower <br /> franchisees. Expert
             consultancy at your <br /> service.
@@ -54,8 +54,8 @@ const Hero = () => {
 
         {/* hero buttton */}
 
-        <div className="px-2 pt-9 font-inter xl:ml-[3.8rem] xl:pt-[2rem] 2xl:ml-[5.2rem] 2xl:px-0 2xl:pt-[3rem]">
-          <button className="contact-us-btn w-full rounded-[12px] px-8 py-3 text-center font-medium text-white xl:max-w-[10rem] 2xl:text-base">
+        <div className="px-2 pt-9 font-inter lg:ml-[2rem] xl:ml-[3.8rem] xl:pt-[2rem] 2xl:ml-[5.2rem] 2xl:px-0 2xl:pt-[3rem]">
+          <button className="contact-us-btn w-full rounded-[12px] px-8 py-3 text-center font-medium text-white lg:max-w-[11rem] xl:max-w-[10rem] 2xl:text-base">
             Contact us
           </button>
         </div>
