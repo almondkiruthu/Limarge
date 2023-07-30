d92b5bae8cfcb5d71a0c38044789caceb97fd0a1
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-07-30 21:04:11 +0300

    I made the hero extension ribbon responsive on medium screens and above

--- client/src/components/Heroextension.tsx
+++ client/src/components/Heroextension.tsx
@@ -5,7 +5,7 @@ const Heroextension = () => {
     <>
       <div className=" relative rounded-t-[2.5rem] bg-customBlue pb-[3.75rem]">
         <div className="hero__extension__container container md:flex md:flex-col-reverse md:items-center md:justify-center lg:flex lg:items-center lg:gap-x-[6rem] xl:gap-x-0">
-          <div className="pb-5 pt-10 md:flex lg:ml-[2rem] lg:mr-auto lg:gap-x-[0.3rem] xl:ml-[4rem] xl:gap-x-[1rem] 2xl:ml-[5rem] 2xl:gap-x-[1.5rem]">
+          <div className="pb-5 pt-10 md:mt-[1rem] md:flex lg:ml-[2rem] lg:mr-auto lg:gap-x-[0.3rem] xl:ml-[4rem] xl:gap-x-[1rem] 2xl:ml-[5rem] 2xl:gap-x-[1.5rem]">
             <img
               className="lg:w-[6rem] xl:w-full"
               src="./Heroextension/AlphabetLogo.svg"
@@ -45,17 +45,17 @@ const Heroextension = () => {
               <img
                 src="./Heroextension/User.svg"
                 alt="HappyClient"
-                className="absolute md:w-[70%] md:top-[-9.6rem] lg:top-[-10.5rem] lg:w-[80%] xl:top-[-12.4rem] xl:w-[90%] 2xl:top-[-13.73rem] 2xl:w-[100%]"
+                className="absolute md:top-[-9.6rem] md:w-[70%] lg:top-[-10.5rem] lg:w-[80%] xl:top-[-12.4rem] xl:w-[90%] 2xl:top-[-13.73rem] 2xl:w-[100%]"
               />
             </div>
           </div>
         </div>
         {/* Hero vibrant ribbon  */}
-        <div className="absolute right-0 lg:right-[-1rem] lg:top-[-19rem] xl:right-[-2rem] xl:top-[-20rem] 2xl:right-0 2xl:top-[-22rem]">
+        <div className="absolute right-0 md:right-[-1rem] md:top-[-15rem] lg:right-[-1rem] lg:top-[-19rem] xl:right-[-2rem] xl:top-[-20rem] 2xl:right-0 2xl:top-[-22rem]">
           <img
             src="./Hero/VibrantHeroLine.svg"
             alt="vibrant ribbon"
-            className="w-[150px] lg:h-[16.64313rem] lg:w-[16.92944rem] xl:h-[17.64313rem] xl:w-[20.92944rem] 2xl:h-[18.64313rem] 2xl:w-[25.92944rem]"
+            className="w-[150px] md:h-[14.64313rem] md:w-[15.92944rem] lg:h-[16.64313rem] lg:w-[16.92944rem] xl:h-[17.64313rem] xl:w-[20.92944rem] 2xl:h-[18.64313rem] 2xl:w-[25.92944rem]"
           />
         </div>
       </div>
