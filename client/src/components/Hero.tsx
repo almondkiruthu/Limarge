e67a17dfce62c2f4186279a19634143ede1bd56d
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-07-31 14:21:07 +0300

    Made the hero responsive on small screens of 320px

--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -3,22 +3,22 @@ import Heroextension from "./Heroextension";
 const Hero = () => {
   return (
     <>
-      <div className="container pb-32 sm:pb-[4rem] sm:pt-[1rem] 2xl:pb-[3rem]">
+      <div className="container pb-[3rem] pt-4 sm:pb-[4rem] sm:pt-[1rem] 2xl:pb-[3rem]">
         <div className="font-extrabold text-customBlue">
           <div className="flex flex-wrap items-baseline justify-start pl-4 lg:mr-[9rem] lg:justify-center xl:mr-[11rem] 2xl:mr-[12rem] 2xl:justify-center 2xl:pl-0">
             <div className="">
-              <h1 className=" pr-4 font-archivo text-[2.625rem] md:text-[3.125rem] lg:text-[4.125rem] xl:text-[5.125rem] 2xl:text-[6.125rem]">
+              <h1 className=" pr-4 font-archivo text-[2rem] sm:text-[2.625rem] md:text-[3.125rem] lg:text-[4.125rem] xl:text-[5.125rem] 2xl:text-[6.125rem]">
                 We elevate and
               </h1>
             </div>
-            <div className="sm:mr-2">
+            <div className="mr-2">
               <img
                 src="./Hero/HeroTextImage1.svg"
                 alt="HeroTextImage1"
-                className="w-[2.7rem] sm:w-[2.5rem] md:w-[3rem] lg:w-[3.625rem] xl:w-[4.625rem] 2xl:w-[5.625rem]"
+                className="w-[1.7rem] sm:w-[2.5rem] md:w-[3rem] lg:w-[3.625rem] xl:w-[4.625rem] 2xl:w-[5.625rem]"
               />
             </div>
-            <h1 className="font-archivo text-[2.625rem] md:text-[3.125rem] lg:text-[4.125rem] xl:text-[5.125rem] 2xl:text-[6.125rem]">
+            <h1 className="font-archivo text-[2rem] sm:text-[2.625rem] md:text-[3.125rem] lg:text-[4.125rem] xl:text-[5.125rem] 2xl:text-[6.125rem]">
               enrich
             </h1>
           </div>
@@ -30,13 +30,13 @@ const Hero = () => {
                 <img
                   src="./Hero/HeroTextImage2.svg"
                   alt="HeroTextImage2"
-                  className="sm:w-[3.5rem] md:w-[4.8rem] lg:w-[5.33606rem] xl:w-[6.43606rem] 2xl:w-[6.43606rem]"
+                  className="w-[2.3rem] sm:w-[3.5rem] md:w-[4.8rem] lg:w-[5.33606rem] xl:w-[6.43606rem] 2xl:w-[6.43606rem]"
                 />
               </div>
-              <h1 className="pr-4 font-archivo text-[2.625rem] md:text-[3.125rem] lg:text-[4.125rem] xl:text-[5.125rem] 2xl:text-[6.125rem]">
+              <h1 className="pr-4 font-archivo text-[2rem] sm:text-[2.625rem] md:text-[3.125rem] lg:text-[4.125rem] xl:text-[5.125rem] 2xl:text-[6.125rem]">
                 change across
               </h1>
-              <h1 className="pr-4 font-archivo text-[2.625rem] md:text-[3.125rem] lg:text-[4.125rem] xl:text-[5.125rem] 2xl:text-[6.125rem]">
+              <h1 className="pr-4 font-archivo text-[2rem] sm:text-[2.625rem] md:text-[3.125rem] lg:text-[4.125rem] xl:text-[5.125rem] 2xl:text-[6.125rem]">
                 industries
               </h1>
             </div>
@@ -46,7 +46,7 @@ const Hero = () => {
         {/* Hero p text */}
 
         <div className="pl-4 pt-5 text-customBlue lg:ml-[2rem] lg:flex xl:ml-[3.5rem] 2xl:ml-[5.5rem] 2xl:pl-0 2xl:pt-[2rem]">
-          <p className="font-inter text-[1.125rem] 2xl:text-[1.25rem]">
+          <p className="font-inter text-[1rem] sm:text-[1.125rem] 2xl:text-[1.25rem]">
             Unleash growth potential. Empower <br /> franchisees. Expert
             consultancy at your <br /> service.
           </p>
