3ffbb78b1383191bd419f06ff76451e53537d90c
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-08-08 23:42:46 +0300

    I refactored the code in the testimonials  component

--- client/src/components/Testimonials.tsx
+++ client/src/components/Testimonials.tsx
@@ -1,25 +1,23 @@
 //@ts-ignore
 import { useTestimonialContext } from "/lib/context";
 
-
 const Testimonials = () => {
-  const { currentTestimonial, handlePrevTestimonial, handleNextTestimonial } = useTestimonialContext();
+  const { currentTestimonial, handlePrevTestimonial, handleNextTestimonial } =
+    useTestimonialContext();
   return (
     <>
-      <div className="testimonial-card  text-white absolute rounded-[40px] z-10 p-5 lg:p-20 left-3 lg:left-[274px] bottom-[-320px] lg:bottom-[0] w-[95%] lg:w-[70%]">
+      <div className="testimonial-card  absolute bottom-[-320px] left-3 z-10 w-[95%] rounded-[40px] p-5 text-white lg:bottom-[0] lg:left-[274px] lg:w-[70%] lg:p-20">
         <div>
-          <h3 className="font-archivo font-semibold text-center lg:text-start text-[40px] lg:text-[58px] pt-8">
+          <h3 className="pt-8 text-center font-archivo text-[40px] font-semibold lg:text-start lg:text-[58px]">
             Igniting success through <br /> consultancy expertise
           </h3>
         </div>
         <div className="flex flex-wrap lg:gap-72">
           <div className="font-archivo text-[22px] lg:text-[28px]">
-            <p className="pb-8 pt-5">
-            {currentTestimonial.message}
-            </p>
+            <p className="pb-8 pt-5">{currentTestimonial.message}</p>
           </div>
 
-          <div className="absolute right-3 top-3 lg:top-[230px] lg:right-20">
+          <div className="absolute right-3 top-3 lg:right-20 lg:top-[230px]">
             <img
               src="/Testimonial/Quotes.svg"
               alt="quotes"
@@ -30,9 +28,11 @@ const Testimonials = () => {
         <div className="flex gap-x-[100px] lg:gap-[800px]">
           <div className="font-inter">
             <p className="text-[20px]">{currentTestimonial.clientName}</p>
-            <p className="text-base opacity-80">{currentTestimonial.companyName}</p>
+            <p className="text-base opacity-80">
+              {currentTestimonial.companyName}
+            </p>
           </div>
-          <div className="flex gap-4 items-center">
+          <div className="flex items-center gap-4">
             <button onClick={handlePrevTestimonial}>
               <img src="/Testimonial/ArrowLeft.svg" alt="prev" />
             </button>
