4e9a43e27b79d0a8ec820545efb564daccc5f6ba
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-08-30 14:34:08 +0300

    I added the container class to the other services cards

--- client/src/components/Servicesextension.tsx
+++ client/src/components/Servicesextension.tsx
@@ -3,11 +3,15 @@ import OtherServicescards from "./OtherServicescards";
 const Servicesextension = () => {
   return (
     <>
-      <div className="pt-20 lg:pt-52">
-        <div className="text-center pb-10">
-          <h3 className="font-archivo text-customBlue text-[40px] lg:text-[58px] font-semibold">Other Services</h3>
+      <div className="container">
+        <div className="pt-20 lg:pt-52">
+          <div className="pb-10 text-center">
+            <h3 className="font-archivo text-[40px] font-semibold text-customBlue lg:text-[58px]">
+              Other Services
+            </h3>
+          </div>
+          <OtherServicescards />
         </div>
-        <OtherServicescards />
       </div>
     </>
   );
