e05fca00cf2a01df0c1d0b6f79d5ba58b6fd39d1
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-08-31 13:11:00 +0300

    I made the first part of the stories component responsive

--- client/src/components/Stories.tsx
+++ client/src/components/Stories.tsx
@@ -6,22 +6,22 @@ const Stories = () => {
       <div className="relative rounded-b-[2.5rem] bg-customBlue pb-96">
         <div className="container">
           <div className="pt-[6.25rem] text-center">
-            <h2 className="font-archivo text-[2.5rem] font-semibold text-white 2xl:text-[3.625rem]">
+            <h2 className="font-archivo text-[2.5rem] font-semibold text-white xl:text-[3.625rem]">
               Years of strategic <br /> consulting expertise
             </h2>
           </div>
-          <div className="invisible 2xl:visible 2xl:absolute 2xl:right-0 2xl:top-[4.375rem]">
+          <div className="invisible xl:visible xl:absolute xl:right-0 xl:top-[4.375rem]">
             <img
               src="/Storiesassets/Ring.svg"
               alt="Ring"
-              className="h-[0] 2xl:h-[100%]"
+              className="h-[0] xl:h-[10rem] 2xl:h-[100%]"
             />
           </div>
-          <div className="flex flex-wrap gap-y-10 pt-10 2xl:gap-x-[8rem] 2xl:gap-y-0 2xl:pt-24">
+          <div className="flex flex-wrap gap-y-10 pt-10  xl:gap-x-[2rem] xl:gap-y-0 xl:pt-28  2xl:gap-x-[8rem] 2xl:pt-24">
             <div className="mx-auto rounded-[1.33rem] bg-white">
-              <div className="px-[3.75rem] py-4  text-customBlue 2xl:px-[4.6875rem] 2xl:py-8">
+              <div className="px-[3.75rem] py-4  text-customBlue xl:px-[4.6875rem] xl:py-8">
                 <div>
-                  <p className="font-archivo text-[3rem] font-extrabold 2xl:text-[4.875rem]">
+                  <p className="font-archivo text-[3rem] font-extrabold xl:text-[4.875rem]">
                     15
                   </p>
                 </div>
@@ -33,9 +33,9 @@ const Stories = () => {
               </div>
             </div>
             <div className="mx-auto rounded-[1.33rem] bg-white">
-              <div className="px-[3.75rem] py-4  text-customBlue 2xl:px-[4.6875rem] 2xl:py-8">
+              <div className="px-[3.75rem] py-4  text-customBlue xl:px-[4.6875rem] xl:py-8">
                 <div>
-                  <p className="font-archivo text-[3rem] font-extrabold 2xl:text-[4.875rem]">
+                  <p className="font-archivo text-[3rem] font-extrabold xl:text-[4.875rem]">
                     2
                   </p>
                 </div>
@@ -45,9 +45,9 @@ const Stories = () => {
               </div>
             </div>
             <div className="mx-auto rounded-[1.33rem]  bg-white">
-              <div className="px-[3.9375rem] py-4  text-customBlue 2xl:px-[4.6875rem] 2xl:py-8">
+              <div className="px-[3.9375rem] py-4  text-customBlue xl:px-[4.6875rem] xl:py-8">
                 <div>
-                  <p className="font-archivo text-[3rem] font-extrabold 2xl:text-[4.875rem]">
+                  <p className="font-archivo text-[3rem] font-extrabold xl:text-[4.875rem]">
                     3K+
                   </p>
                 </div>
@@ -62,7 +62,7 @@ const Stories = () => {
           </div>
 
           <div className="px-4 pt-[3.125rem] text-center font-inter 2xl:pt-20">
-            <button className="contact-us-btn w-[100%] rounded-[0.75rem] py-3 text-center font-medium text-white lg:w-[10%] lg:px-8">
+            <button className="contact-us-btn w-[100%] rounded-[0.75rem] py-3 text-center font-medium text-white lg:px-8 xl:w-[15%] 2xl:w-[10%]">
               Contact us
             </button>
           </div>
