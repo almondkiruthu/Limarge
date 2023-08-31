bc5a0599971013840a3d700cdc5bb9b03d1907d1
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-08-31 13:21:00 +0300

    Made the stories component responsive on lg screens

--- client/src/components/Stories.tsx
+++ client/src/components/Stories.tsx
@@ -62,7 +62,7 @@ const Stories = () => {
           </div>
 
           <div className="px-4 pt-[3.125rem] text-center font-inter 2xl:pt-20">
-            <button className="contact-us-btn w-[100%] rounded-[0.75rem] py-3 text-center font-medium text-white lg:px-8 xl:w-[15%] 2xl:w-[10%]">
+            <button className="contact-us-btn w-[100%] rounded-[0.75rem] py-3 text-center font-medium text-white lg:px-8 lg:w-[16%] xl:w-[15%] 2xl:w-[10%]">
               Contact us
             </button>
           </div>
