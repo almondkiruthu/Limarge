3982a8ac9602151b3353f855bf4a84a44fe5dec6
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-09-02 23:28:19 +0300

    Changed the text from 32px to 2rem

--- client/src/components/Team.tsx
+++ client/src/components/Team.tsx
@@ -37,7 +37,7 @@ const Team = () => {
                 <br />
                 B/ed(special education)
               </p>
-              <p className="font-inter text-[1.5rem] lg:text-[32px]">
+              <p className="font-inter text-[1.5rem] lg:text-[2rem]">
                 LEAD CONSULTANT
               </p>
             </div>
