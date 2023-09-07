ee95d61df37c7071cd9280a75448391ede0aecda
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-09-07 20:58:53 +0300

    Changed the text from 24px to 1.5rem

--- client/src/components/Accordion.tsx
+++ client/src/components/Accordion.tsx
@@ -98,7 +98,7 @@ export default function Accordion() {
         <Disclosure as="div" className="mt-1">
           {({ open }) => (
             <>
-              <Disclosure.Button className=" font-archivo font-semibold flex w-full justify-between rounded-lg px-4 py-2 text-left text-[24px] lg:text-[28px] text-white focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
+              <Disclosure.Button className=" font-archivo font-semibold flex w-full justify-between rounded-lg px-4 py-2 text-left text-[1.5rem] lg:text-[28px] text-white focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
                 <span>What if my clients have technical help?</span>
                 <ChevronUpIcon
                   className={`${
