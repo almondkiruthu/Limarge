77c1c4940a5ba3c1a465e4b12c0b9d80763c8fa6
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-09-11 23:50:51 +0300

    refactored the code in the teams component

--- client/src/components/Team.tsx
+++ client/src/components/Team.tsx
@@ -1,7 +1,7 @@
 const Team = () => {
   return (
     <>
-      <div className="container relative pb-[rem] pt-0">
+      <div className="container relative">
         <div>
           <p className="text-center font-archivo text-[2.5rem] font-semibold text-customBlue lg:text-[3.625rem] ">
             Comprised of diverse <br /> minds
